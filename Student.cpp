3ff8d79b7929b9341d93ed4f5bacda73e7b750cd
sfsy1 <sfsy1@yahoo.com>
2017-12-20 23:59:37 -0500

    Add files via upload

--- Student.cpp
+++ Student.cpp
@@ -1,26 +1,52 @@
-#include <stdio.h>
 #include "Student.h"
+#include <string>
+#include <stdio.h>
+#include <sstream>
+#include <sstream>
+#include <vector>
+#include <numeric>
+#include <functional>
 
-Student::Student(std::string fn, std::string ln, int y, Major m)
-  : _first_name(fn), _last_name(ln), _grad_year(y), _major(m) {}
+Student::Student(std::string fn, std::string ln, int y, Major m) {
+  _grad_year = y;
+  _first_name = fn;
+  _last_name = ln;
+  _major = m;
+  std::vector<float> _grades;
+}
 
 Student::~Student() {}
 
-float Student::getGpa() {
-  // TODO Fill in real implementation
-  return 0.0;
-}
-
+//add grade to vector
 void Student::addGrade(float grade) {
-  // TODO Fill in real implementation
+	_grades.push_back(grade);
 }
 
+//calc average GPA
+float Student::getGpa() {
+	return accumulate(_grades.begin(), _grades.end(), 0.00)/ _grades.size();
+}
+
+//convert majors to string
+std::string Student::majorString(Major m) {
+	switch(m) {
+		case Major::EE: return "EE";
+		case Major::ME: return "ME";
+		case Major::CE: return "CE";
+		case Major::CHE: return "CHE";
+		case Major::BSE: return "BSE";
+		case Major::ART: return "ART";
+		case Major::ARCH: return "ARCH";
+		default: return "Invalid Major";
+	}
+}
+
+//get last name of student
 const std::string& Student::getLastName() {
-  // TODO Fill in real implementation
-  return std::string();
-}
+  return _last_name;
+}		
 
+//print
 void Student::printInfo() {
-  // TODO Fill in real implementation
-  printf("Student::printInfo() not implemented\n");
+	printf("%s, %s\n%s %d\nGPA: %1.2f\n", _last_name.c_str(), _first_name.c_str(), majorString(_major).c_str(), _grad_year, getGpa());
 }
--- StudentDatabase.cpp
+++ StudentDatabase.cpp
@@ -1,13 +1,29 @@
 #include "StudentDatabase.h"
+#include <map>
+#include <iostream>
+
 
 // Insert a student into _students map
 void StudentDatabase::addStudent(Student *s) {
-  // TODO Implement this
+	_students[s->getLastName()] = s;
 }
 
 // Find the student with the matching last_name
 // and call printInfo on that student, or
 // print "<last_name> not found" if the student is not found.
 void StudentDatabase::printStudent(std::string last_name) {
-  // TODO Implement this
+	if (_students.find(last_name) == _students.end()){ 
+	std::cout << last_name << " not found" << std::end;
+  }
+  else
+    _students[last_name]->printInfo(); 
+  return;
+  // auto thisstudent = _students.find(last_name);
+  // if (thisstudent == _students.end()){
+	  // printf("%s not found\n", last_name.c_str());
+  // }
+  // else {
+	  // _students[last_name]->Student::printInfo();
+  // }
+  // return;
 }
